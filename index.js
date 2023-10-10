72ad1fbfaae58dccf70f0974a40fb688e3dea973
prashantSingh <prashantsing13052000@gmail.com>
2023-10-10 23:08:39 +0530

    completed except dropdown

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const togle = document.querySelector('#paraBtn');
-console.log(togle);
 const txt = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, qui. Doloribus eveniet reprehenderit magnam nobis repellat ullam dolorem nulla, at debitis aliquid, doloremque unde provident vitae cupiditate sapiente. Eum, tempore? Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc scelerisque viverra mauris in aliquam sem fringilla ut. Faucibus ornare suspendisse sed nisi lacus sed viverra. Dictum fusce ut placerat orci nulla. Quam vulputate dignissim suspendisse in est ante. Sed elementum tempus egestas sed sed risus pretium quam vulputate. Orci ac auctor augue mauris augue neque gravida. Venenatis urna cursus eget nunc scelerisque viverra. Urna et pharetra pharetra massa massa ultricies mi quis hendrerit. Erat velit scelerisque in dictum non consectetur a erat nam. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Bibendum neque egestas congue quisque egestas diam in arcu. Nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Risus in hendrerit gravida rutrum. Blandit turpis cursus in hac habitasse platea dictumst quisque sagittis. Suspendisse in est ante in nibh mauris cursus mattis molestie. Et leo duis ut diam quam nulla porttitor massa id. Posuere morbi leo urna molestie at. Ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Nunc sed velit dignissim sodales. Bibendum ut tristique et egestas quis ipsum."
 const para = document.querySelector('#para');
 togle.addEventListener('click', () => {
@@ -19,28 +18,7 @@ for (let i of arr) {
     str += `<img src="${i}" class="image" alt="Failed to load"  >`
 }
 image.innerHTML = str;
-// const prev = document.querySelector('#prev');
-// const nxt = document.querySelector('#next');
-// let idx = 0;
-// prev.addEventListener('click', () => {
-//     if (idx === 0) {
-//         idx = 4;
-//     }
-//     else {
-//         idx--;
-//     }
-//     image.setAttribute('src', arr[idx]);
-// });
-// nxt.addEventListener('click', () => {
-//     if (idx === 4) {
-//         idx = 0;
-//     }
-//     else {
-//         idx++;
-//     }
-//     image.setAttribute('src', arr[idx]);
-// });
-// image.setAttribute('src', arr[idx]);
+
 
 
 const ul = document.querySelector('#temp')
@@ -54,13 +32,9 @@ for (let i of imageContainer) {
         const newNode = document.createElement('img');
         newNode.setAttribute('src', e.target.src);
         newNode.setAttribute('class', 'viewerImage');
-
-        // // // ul.lastChild = `<img src="${e.target.src} id="upperImage" alt="Failed to load" width="50%" height="50%" >`;
-        // console.dir(ul.lastElementChild);
         for (let j = 0; j < arr.length; j++) {
             if (e.target.src === arr[j]) {
                 idx = j;
-                console.log(idx);
                 break;
             }
         }
